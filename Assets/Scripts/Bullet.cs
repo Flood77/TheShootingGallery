3e5a7ca6322e57b6d1ce82f6f1b706f803fd102a
Matthew Mason <matthewmason7@outlook.com>
2021-01-13 21:47:22 -0700

    Initial Setup
    Initial upload of project

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public void Fire(Vector3 forward)
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(forward * speed, ForceMode.VelocityChange);
+    }
+}
